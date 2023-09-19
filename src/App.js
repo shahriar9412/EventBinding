fe65c97365f5537d6bca52eac7330efebd433130
shahriar-hossain <shahriar9412@gmail.com>
2023-09-19 17:18:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventBinding from './EventBinding'
+
+const App = () => {
+  return (
+    <div>
+        <EventBinding />
+    </div>
+  )
+}
+
+export default App
